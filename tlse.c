4d32ffad556fe77339178a157a455b6019d57734
Eduard Suica <eduard.suica@gmail.com>
2016-03-22 08:06:01 +0200

    Update tlse.c

--- tlse.c
+++ tlse.c
@@ -367,6 +367,7 @@ typedef struct {
     const char *Gx;
     const char *Gy;
     const char *order;
+    int cofactor;
 } ECCCurveParameters;
 
 static const ECCCurveParameters secp192r1 = {
@@ -378,7 +379,8 @@ static const ECCCurveParameters secp192r1 = {
     "64210519E59C80E70FA7E9AB72243049FEB8DEECC146B9B1", // B
     "188DA80EB03090F67CBF20EB43A18800F4FF0AFD82FF1012", // Gx
     "07192B95FFC8DA78631011ED6B24CDD573F977A11E794811", // Gy
-    "FFFFFFFFFFFFFFFFFFFFFFFF99DEF836146BC9B1B4D22831"  // order (n)
+    "FFFFFFFFFFFFFFFFFFFFFFFF99DEF836146BC9B1B4D22831", // order (n)
+    1 // cofactor
 };
 
 
@@ -391,7 +393,8 @@ static const ECCCurveParameters secp224r1 = {
     "B4050A850C04B3ABF54132565044B0B7D7BFD8BA270B39432355FFB4", // B
     "B70E0CBD6BB4BF7F321390B94A03C1D356C21122343280D6115C1D21", // Gx
     "BD376388B5F723FB4C22DFE6CD4375A05A07476444D5819985007E34", // Gy
-    "FFFFFFFFFFFFFFFFFFFFFFFFFFFF16A2E0B8F03E13DD29455C5C2A3D"  // order (n)
+    "FFFFFFFFFFFFFFFFFFFFFFFFFFFF16A2E0B8F03E13DD29455C5C2A3D", // order (n)
+    1 // cofactor
 };
 
 static const ECCCurveParameters secp224k1 = {
@@ -403,7 +406,8 @@ static const ECCCurveParameters secp224k1 = {
     "00000000000000000000000000000000000000000000000000000005", // B
     "A1455B334DF099DF30FC28A169A467E9E47075A90F7E650EB6B7A45C", // Gx
     "7E089FED7FBA344282CAFBD6F7E319F7C0B0BD59E2CA4BDB556D61A5", // Gy
-    "0000000000000000000000000001DCE8D2EC6184CAF0A971769FB1F7"  // order (n)
+    "0000000000000000000000000001DCE8D2EC6184CAF0A971769FB1F7", // order (n)
+    1 // cofactor
 };
 
 static const ECCCurveParameters secp256r1 = {
@@ -415,7 +419,8 @@ static const ECCCurveParameters secp256r1 = {
     "5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B", // B
     "6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296", // Gx
     "4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5", // Gy
-    "FFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551"  // order (n)
+    "FFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551", // order (n)
+    1 // cofactor
 };
 
 static const ECCCurveParameters secp256k1 = {
@@ -427,7 +432,8 @@ static const ECCCurveParameters secp256k1 = {
     "0000000000000000000000000000000000000000000000000000000000000007", // B
     "79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", // Gx
     "483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", // Gy
-    "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141"  // order (n)
+    "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", // order (n)
+    1 // cofactor
 };
 
 static const ECCCurveParameters secp384r1 = {
@@ -439,7 +445,8 @@ static const ECCCurveParameters secp384r1 = {
     "B3312FA7E23EE7E4988E056BE3F82D19181D9C6EFE8141120314088F5013875AC656398D8A2ED19D2A85C8EDD3EC2AEF", // B
     "AA87CA22BE8B05378EB1C71EF320AD746E1D3B628BA79B9859F741E082542A385502F25DBF55296C3A545E3872760AB7", // Gx
     "3617DE4A96262C6F5D9E98BF9292DC29F8F41DBD289A147CE9DA3113B5F0B8C00A60B1CE1D7E819D7A431D7C90EA0E5F", // Gy
-    "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC7634D81F4372DDF581A0DB248B0A77AECEC196ACCC52973"  // order (n)
+    "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC7634D81F4372DDF581A0DB248B0A77AECEC196ACCC52973", // order (n)
+    1 // cofactor
 };
 
 static const ECCCurveParameters secp521r1 = {
@@ -451,7 +458,8 @@ static const ECCCurveParameters secp521r1 = {
     "0051953EB9618E1C9A1F929A21A0B68540EEA2DA725B99B315F3B8B489918EF109E156193951EC7E937B1652C0BD3BB1BF073573DF883D2C34F1EF451FD46B503F00", // B
     "00C6858E06B70404E9CD9E3ECB662395B4429C648139053FB521F828AF606B4D3DBAA14B5E77EFE75928FE1DC127A2FFA8DE3348B3C1856A429BF97E7E31C2E5BD66", // Gx
     "011839296A789A3BC0045C8A5FB42C7D1BD998F54449579B446817AFBD17273E662C97EE72995EF42640C550B9013FAD0761353C7086A272C24088BE94769FD16650", // Gy
-    "01FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA51868783BF2F966B7FCC0148F709A5D03BB5C9B8899C47AEBB6FB71E91386409"  // order (n)
+    "01FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA51868783BF2F966B7FCC0148F709A5D03BB5C9B8899C47AEBB6FB71E91386409", // order (n)
+    1 // cofactor
 };
 
 static const ECCCurveParameters *default_curve = &secp256r1;
@@ -794,8 +802,14 @@ unsigned char *__private_tls_decrypt_ecc_dhe(TLSContext *context, const unsigned
     ecc_key client_key;
     memset(&client_key, 0, sizeof(client_key));
     if (ecc_ansi_x963_import_ex(buffer, len, &client_key, &dp)) {
-        DEBUG_PRINT("Error importic ECC DHE key\n");
+        DEBUG_PRINT("Error importing ECC DHE key\n");
         return NULL;
+    }
+    if (curve->cofactor != 1) {
+        if (mp_set(client_key.pubkey.z, curve->cofactor) != CRYPT_OK) {
+            DEBUG_PRINT("Error setting ECC DHE cofactor\n");
+            return NULL;
+        }
     }
     unsigned char *out = (unsigned char *)TLS_MALLOC(len);
     unsigned long out_size = len;
@@ -1184,7 +1198,7 @@ error:
     return err;
 }
 
-int __private_tls_ecc_import_key(const unsigned char *private_key, int private_len, const unsigned char *public_key, int public_len, ecc_key *key, const ltc_ecc_set_type *dp) {
+int __private_tls_ecc_import_key(const unsigned char *private_key, int private_len, const unsigned char *public_key, int public_len, ecc_key *key, const ECCCurveParameters *curve) {
     //return ecc_import_ex(buffer, len, key, dp);
     unsigned long key_size;
     unsigned char flags[1];
@@ -1193,6 +1207,16 @@ int __private_tls_ecc_import_key(const unsigned char *private_key, int private_l
     LTC_ARGCHK(key != NULL);
     LTC_ARGCHK(ltc_mp.name != NULL);
     key->type = PK_PRIVATE;
+
+    ltc_ecc_set_type dp;
+    memset(&dp, 0, sizeof(dp));
+    dp.B = (char *)curve->B;
+    dp.size = curve->size;
+    dp.name = (char *)curve->name;
+    dp.prime = (char *)curve->P;
+    dp.Gx = (char *)curve->Gx;
+    dp.Gy = (char *)curve->Gy;
+    dp.order = (char *)curve->order;
 
     /* init key */
     if (mp_init_multi(&key->pubkey.x, &key->pubkey.y, &key->pubkey.z, &key->k, NULL) != CRYPT_OK)
@@ -1218,10 +1242,10 @@ int __private_tls_ecc_import_key(const unsigned char *private_key, int private_l
     }
 
     key->idx = -1;
-    key->dp  = dp;
+    key->dp  = &dp;
 
     /* set z */
-    if ((err = mp_set(key->pubkey.z, 1)) != CRYPT_OK) {
+    if ((err = mp_set(key->pubkey.z, curve->cofactor)) != CRYPT_OK) {
         mp_clear_multi(key->pubkey.x, key->pubkey.y, key->pubkey.z, key->k, NULL);
         return err; 
     }
@@ -1281,18 +1305,8 @@ int __private_tls_sign_ecdsa(TLSContext *context, unsigned int hash_type, const 
     ecc_key key;
     int err;
 
-    ltc_ecc_set_type dp;
-    memset(&dp, 0, sizeof(dp));
-    dp.B = (char *)curve->B;
-    dp.size = curve->size;
-    dp.name = (char *)curve->name;
-    dp.prime = (char *)curve->P;
-    dp.Gx = (char *)curve->Gx;
-    dp.Gy = (char *)curve->Gy;
-    dp.order = (char *)curve->order;
-
     // broken ... fix this
-    if (__private_tls_ecc_import_key(context->private_key->priv, context->private_key->priv_len, context->private_key->pk, context->private_key->pk_len, &key, &dp)) {
+    if (__private_tls_ecc_import_key(context->private_key->priv, context->private_key->priv_len, context->private_key->pk, context->private_key->pk_len, &key, curve)) {
         DEBUG_PRINT("Error importing ECC certificate (code: %i)", (int)err);
         return TLS_GENERIC_ERROR;
     }
