0af0122fd6f517cc68b1fa6c2cf2dd76cb2baf1e
Eduard Suica <eduard.suica@gmail.com>
2016-03-25 09:23:57 +0200

    Now supporting ECDSA

--- tlse.c
+++ tlse.c
@@ -53,7 +53,8 @@
 #define TLS_FORWARD_SECRECY
 // support client-side ECDHE
 #define TLS_CLIENT_ECDHE
-// suport ecdsa (not working yet)
+// suport ecdsa: working, but strict. This means that you cannot
+// yet use RSA and ECDSA at the same time (fix this)
 // #define TLS_ECDSA_SUPPORTED
 
 #define TLS_DH_DEFAULT_P            "87A8E61DB4B6663CFFBBD19C651959998CEEF608660DD0F25D2CEED4435E3B00E00DF8F1D61957D4FAF7DF4561B2AA3016C3D91134096FAA3BF4296D830E9A7C209E0C6497517ABD5A8A9D306BCF67ED91F9E6725B4758C022E0B1EF4275BF7B6C5BFC11D45F9088B941F54EB1E59BB8BC39A0BF12307F5C4FDB70C581B23F76B63ACAE1CAA6B7902D52526735488A0EF13C6D9A51BFA4AB3AD8347796524D8EF6A167B5A41825D967E144E5140564251CCACB83E6B486F6B3CA3F7971506026C0B857F689962856DED4010ABD0BE621C3A3960A54E710C375F26375D7014103A4B54330C198AF126116D2276E11715F693877FAD7EF09CADB094AE91E1A1597"
@@ -1631,6 +1632,9 @@ int __private_tls_key_length(TLSContext *context) {
         case TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA:
         case TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256:
         case TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:
+        case TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:
+        case TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256:
+        case TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:
             return 16;
         case TLS_RSA_WITH_AES_256_CBC_SHA:
         case TLS_RSA_WITH_AES_256_CBC_SHA256:
@@ -1640,6 +1644,9 @@ int __private_tls_key_length(TLSContext *context) {
         case TLS_DHE_RSA_WITH_AES_256_GCM_SHA384:
         case TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA:
         case TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:
+        case TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA:
+        case TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384:
+        case TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:
             return 32;
     }
     return 0;
@@ -1653,10 +1660,18 @@ int __private_tls_is_aead(TLSContext *context) {
         case TLS_DHE_RSA_WITH_AES_256_GCM_SHA384:
         case TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:
         case TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:
+        case TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:
+        case TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256:
+        case TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:
+        case TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA:
+        case TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384:
+        case TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:
             return 1;
     }
     return 0;
 }
+
+
 
 unsigned int __private_tls_mac_length(TLSContext *context) {
     switch (context->cipher) {
@@ -1666,6 +1681,8 @@ unsigned int __private_tls_mac_length(TLSContext *context) {
         case TLS_DHE_RSA_WITH_AES_256_CBC_SHA:
         case TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA:
         case TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA:
+        case TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:
+        case TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA:
             return __TLS_SHA1_MAC_SIZE;
         case TLS_RSA_WITH_AES_128_CBC_SHA256:
         case TLS_RSA_WITH_AES_256_CBC_SHA256:
@@ -1675,10 +1692,14 @@ unsigned int __private_tls_mac_length(TLSContext *context) {
         case TLS_DHE_RSA_WITH_AES_128_GCM_SHA256:
         case TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256:
         case TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:
+        case TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256:
+        case TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:
             return __TLS_SHA256_MAC_SIZE;
         case TLS_RSA_WITH_AES_256_GCM_SHA384:
         case TLS_DHE_RSA_WITH_AES_256_GCM_SHA384:
         case TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:
+        case TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384:
+        case TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:
             return __TLS_SHA384_MAC_SIZE;
     }
     return 0;
@@ -1693,9 +1714,11 @@ int __private_tls_expand_key(TLSContext *context) {
     int key_length = __private_tls_key_length(context);
     int mac_length = __private_tls_mac_length(context);
     
-    if ((!key_length) || (!mac_length))
+    if ((!key_length) || (!mac_length)) {
+        DEBUG_PRINT("KEY EXPANSION FAILED, KEY LENGTH: %i, MAC LENGTH: %i\n", key_length, mac_length);
         return 0;
-    
+    }
+
     if (context->is_server)
         __private_tls_prf(context, key, sizeof(key), context->master_key, context->master_key_len, (unsigned char *)"key expansion", 13, context->local_random, __TLS_SERVER_RANDOM_SIZE, context->remote_random, __TLS_CLIENT_RANDOM_SIZE);
     else
@@ -2336,7 +2359,7 @@ int __private_tls_crypto_create(TLSContext *context, int key_length, int iv_leng
     }
     
     int cipherID = find_cipher("aes");
-    DEBUG_PRINT("Using cipher ID: %i\n", cipherID);
+    DEBUG_PRINT("Using cipher ID: %x\n", (int)context->cipher);
     if (__private_tls_is_aead(context)) {
         int res1 = gcm_init(&context->crypto.aes_gcm_local, cipherID, localkey, key_length);
         int res2 = gcm_init(&context->crypto.aes_gcm_remote, cipherID, remotekey, key_length);
@@ -3253,9 +3276,9 @@ const char *tls_cipher_name(TLSContext *context) {
             case TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384:
                 return "ECDHE-ECDSA-AES256CBC-SHA384";
             case TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:
-                return "ECDHE-ECDSA-AES128CBC-SHA256";
+                return "ECDHE-ECDSA-AES128GCM-SHA256";
             case TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:
-                return "ECDHE-ECDSA-AES256CBC-SHA384";
+                return "ECDHE-ECDSA-AES256GCM-SHA384";
         }
     }
     return "UNKNOWN";
