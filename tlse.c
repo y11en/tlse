0be255c1fd0cbc60bf652a3d18b9c4d5bc5d956a
Eduard Suica <eduardsui@users.noreply.github.com>
2018-07-13 02:21:51 +0300

    Update tlse.c

--- tlse.c
+++ tlse.c
@@ -4722,19 +4722,6 @@ int tls_cipher_is_fs(struct TLSContext *context, unsigned short cipher) {
             case TLS_AES_256_GCM_SHA384:
             case TLS_CHACHA20_POLY1305_SHA256:
                 return 1;
-            case TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:
-            case TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256:
-            case TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:
-                if ((context) && (context->certificates) && (context->certificates_count) && (context->ec_private_key))
-                    return 1;
-                return 0;
-            case TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:
-            case TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256:
-            case TLS_DHE_RSA_WITH_AES_128_GCM_SHA256:
-            case TLS_DHE_RSA_WITH_CHACHA20_POLY1305_SHA256:
-                if ((context) && (context->certificates) && (context->certificates_count) && (context->private_key))
-                    return 1;
-                return 0;
         }
         return 0;
     }
